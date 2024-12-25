1aec7c29043587fa690580354127f36103ca7900
Kavindu Harshana <45476434+harshanacz@users.noreply.github.com>
2024-12-25 23:03:24 +0530

    -superscrapertest

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ import cloudscraper
 from bs4 import BeautifulSoup
 
 scraper = cloudscraper.create_scraper()
-url = 'https://jumpbooks.lk/bestselling-books-in-sri-lanka/'
-response = scraper.get(url)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.title.text)
+processing = True
+pageNo = 1
+
+while processing:
+    url = "https://www.daraz.lk/womens-fashion/?page=" + str(pageNo)
+    response = scraper.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    if pageNo > 25:
+        processing = False
+    else:
+        allProducts = soup.find_all("div", class_="Bm3ON")
+        for product in allProducts:
+            item = {}
+            item['Title'] = product.find('img').attrs.get("alt", "No Title")
+            print(item["Title"])
+
+    pageNo += 1  
